e683255650f9b49660d13c6eba75b2414f376c62
Coding-Is-Fun.org <64811072+codingisfun2@users.noreply.github.com>
2020-07-10 15:51:26 -0400

    Add files via upload

---
+++ code_template.py
@@ -0,0 +1,22 @@
+"""
+******************************
+This program is to [add the purpose of your program here]
+******************************
+Author: [your name]
+Date: [your date]
+Functions: [your functions]
+Version: [your version number]
+"""
+# import [your package]
+
+
+# main function of the program
+def main():
+    # initializing
+    # your to do
+    print("Copy this source code as your starting point")
+
+
+# Runs the program by calling the main() function
+if __name__ == '__main__':
+    main()
